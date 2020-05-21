4207fb4765800eff8a90e08f85fe270c82868d94
Todd Spainhour <toddspainhour@gmail.com>
2020-05-20 22:16:36 -0500

    player cards printing

--- src/components/RosterContainer/RosterContainer.js
+++ src/components/RosterContainer/RosterContainer.js
@@ -1,15 +1,35 @@
 import React from 'react';
-
 import './RosterContainer.scss';
 
+import rosterData from '../../helpers/data/rosterData';
+import authData from '../../helpers/data/authData';
+
+import Roster from '../Roster/Roster';
+
 class RosterContainer extends React.Component {
-  render() {
-    return (
-      <div className="RosterContainer">
-        <h2>This is your Roster Container</h2>
+state = {
+  roster: [],
+}
+
+componentDidMount() {
+  rosterData.getRosterByUid(authData.getUid())
+    .then((roster) => this.setState({ roster }))
+    .catch((err) => console.error('unable to get roster', err));
+  console.error('inside your componentDidMount in RosterContainer');
+}
+
+render() {
+  const { roster } = this.state;
+  const makeRoster = roster.map((player) => <Roster key={player.id} roster={player} />); // having player or roster could cause issues...
+
+  return (
+    <div className="RosterContainer">
+      <div className="d-flex flex-wrap">
+        {makeRoster}
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default RosterContainer;
