725bd2500af9684b1666b0ab9d9104318da590b6
Todd Spainhour <toddspainhour@gmail.com>
2020-05-23 11:45:08 -0500

    delete players functional

--- src/helpers/data/rosterData.js
+++ src/helpers/data/rosterData.js
@@ -3,20 +3,18 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-console.error('your baseUrl is ...', baseUrl + '/roster.json');
+// console.error('your baseUrl is ...', baseUrl + '/roster.json');
 
 const getRosterByUid = (uid) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/roster.json?orderBy="uid"&equalTo="${uid}"`)
     .then((result) => {
       const allRosterObject = result.data;
-      console.error('result.data displays? ', allRosterObject);
       const roster = [];
       if (allRosterObject !== null) {
         Object.keys(allRosterObject).forEach((rosterId) => {
           const newRoster = allRosterObject[rosterId];
           newRoster.id = rosterId;
           roster.push(newRoster);
-          console.error('this is inside your getRosterByUid in rosterData');
         });
       }
       resolve(roster);
@@ -24,4 +22,8 @@ const getRosterByUid = (uid) => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
-export default { getRosterByUid };
+
+const deletePlayer = (rosterId) => axios.delete(`${baseUrl}/roster/${rosterId}.json`); // this playerId might cause issues
+
+
+export default { getRosterByUid, deletePlayer };
